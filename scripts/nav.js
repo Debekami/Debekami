d7d70cd045f7d8b5fcf40a44e45ad190e2061387
Marcin <mdebowski78@gmail.com>
2021-05-19 18:16:31 +0200

    Added new categories & some changes

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,16 +1,24 @@
 var aboutme = function() {
-	window.location.href = "../aboutme/index.html";
+	window.location.href = "./index.html";
 }
 var projects = function() {
-	window.location.href = "../projects/index.html";
+	window.location.href = "./projects.html"; 
 }
 var github = function() {
 	window.location.href = "https://github.com/Debekami";
 }
 var contact = function() {
-	window.location.href = "../contact/index.html";
+	window.location.href = "./contact.html";
+}
+var projects_drawings = function() {
+	window.location.href = "./drawings.html";
+}
+var projects_games = function() {
+	window.location.href = "./games.html";
 }
 document.getElementById("aboutme").onclick = aboutme;
 document.getElementById("projects").onclick = projects;
 document.getElementById("github").onclick = github;
 document.getElementById("contact").onclick = contact;
+document.getElementById("drawings").onclick = projects_drawings;
+document.getElementById("games").onclick = projects_games;
